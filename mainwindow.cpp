6f2067e3fb2c286f81e3276f6f42bd6985dd8dfe
notch881039 <himarinn294712@live.jp>
2021-10-27 18:40:14 +0900

    abcd

---
+++ mainwindow.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void){
+    int version_main = 183;
+    char version_extra = "1";
+
+    printf("version_main: %d", version_main);
+    printf("version_extra: %c", version_extra);
+
+    return 0:
+}
